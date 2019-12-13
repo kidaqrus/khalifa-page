f74d8d8e3c85ccc0810379694c516fd9a2c50bce
kidaqrus <adikwusule@gmail.com>
2019-12-13 19:27:04 +0100

    Changed the HTML for the site.

--- Kweb/models.py
+++ Kweb/models.py
@@ -41,3 +41,7 @@ class Testimonial(models.Model):
     Images = models.ImageField(upload_to='images/')
     body = models.CharField(max_length=50)
     
+    
+    def __str__(self):
+        return self.name
+    
