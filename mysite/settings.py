84d3582fd20f7453726949f66cf9d5e1d076f575
kidaqrus <adikwusule@gmail.com>
2020-01-31 19:06:04 +0100

    Changed the HTML settings in settings file.

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["Kweb/templates/home.html"],
+        'DIRS': ["Kweb/templates/Kweb/home.html"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
