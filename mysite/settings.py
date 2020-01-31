31a9a58ef3aa5bffc546ff90f4975969bae36ad9
kidaqrus <adikwusule@gmail.com>
2020-01-31 20:19:27 +0100

    html set

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["Kweb/templates/Kweb/home.html"],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
