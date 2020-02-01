a485ee407c6e0569ea06935b0407ab8008fdee36
kidaqrus <adikwusule@gmail.com>
2020-02-01 16:57:20 +0100

    temp fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
